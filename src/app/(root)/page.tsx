b183db3171df42809b1e04c93da2d7fe76cc95f6
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-05-12 17:26:13 +0500

    Tailwind fixed

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,35 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Home</h1>
+        <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {/* {query ? `Search results for "${query}"` : "All Startups"} */}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {/* {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : ( */}
+            <p className="no-results">No startups found</p>
+          {/* )} */}
+        </ul>
+      </section>
+
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,56 +1,18 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-// import "easymde/dist/easymde.min.css";
-// import ft from '../app/fonts/._._WorkSans-Black.ttf'
 
 const workSans = localFont({
   src: [
-    {
-      path: "../app/fonts/._WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-Thin.ttf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../app/fonts/._WorkSans-ExtraLight.ttf",
-      weight: "100",
-      style: "normal",
-    },
+    { path: "../app/fonts/._WorkSans-Black.ttf", weight: "900", style: "normal" },
+    { path: "../app/fonts/._WorkSans-ExtraBold.ttf", weight: "800", style: "normal" },
+    { path: "../app/fonts/._WorkSans-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../app/fonts/._WorkSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../app/fonts/._WorkSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../app/fonts/._WorkSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../app/fonts/._WorkSans-Thin.ttf", weight: "200", style: "normal" },
+    { path: "../app/fonts/._WorkSans-ExtraLight.ttf", weight: "100", style: "normal" },
   ],
   variable: "--font-work-sans",
 });
